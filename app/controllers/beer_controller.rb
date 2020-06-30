9daf1b5106270033137d56db7578cdc3e2801987
mflem8 <matthewfleming8@gmail.com>
2020-06-30 08:53:11 -0400

    adds patch route

--- app/controllers/beer_controller.rb
+++ app/controllers/beer_controller.rb
@@ -5,6 +5,21 @@ class BeerController < ApplicationController
     erb :'beers/index'
   end
 
+  get '/beers/new' do
+    @users = User.all
+    erb :'/beers/new'
+  end
+
+  post '/beers' do
+    user = User.find_by_id(params[:user_id])
+    beer = user.beers.build(params)
+    if beer.save
+      redirect '/beers/'
+    else
+      redirect '/beers/new'
+    end
+  end
+
   get '/beers/:id' do
     @beer = Beer.find_by_id(params[:id])
 
@@ -12,6 +27,20 @@ class BeerController < ApplicationController
       erb :'/beers/show'
     else
       redirect '/beers'
+    end
+  end
+
+  get '/beers/:id/edit' do
+    @beer = Beer.find_by_id(params[:id])
+    erb :'/beers/edit'
+  end
+
+  patch '/beers/:id' do
+    beer = Beer.find_by_id(params[:id])
+    if beer.update(name: params[:name], style: params[:style], rating: params[:rating])
+      redirect "/beers/#{beer.id}"
+    else
+      redirect "/beers/#{beer}/edit"
     end
   end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,5 @@
 class Beer < ActiveRecord::Base
   belongs_to :user
+  validates :name, :style, :rating, presence: true
 
 end
