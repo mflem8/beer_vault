95b2014347b43866624c82600fe4640549c927db
mflem8 <matthewfleming8@gmail.com>
2020-06-30 21:36:38 -0400

    adds logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,15 @@ class SessionsController < ApplicationController
   post '/login' do
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/beers'
+    else
+      redirect '/login'
+    end
   end
 
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
 end
